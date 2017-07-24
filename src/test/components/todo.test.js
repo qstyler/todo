42bddcd88dcea6fc032cd90f8f53703fcd5e1f35
Kosta <kest.bodnya@gmail.com>
2017-07-24 19:15:42 +0200

    todo test

--- src/test/components/todo.test.js
+++ src/test/components/todo.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import uuid from 'uuid/v4';
 
 import { Todo } from '../../components/Todo/Todo';
 import { SearchTodo } from '../../components/Todo';
 import { AddTodo } from '../../components/Todo';
+import { TodoList } from '../../components/Todo';
 
 describe('Todo test suite', () => {
     it('Todo should be true', () => {
@@ -13,8 +13,10 @@ describe('Todo test suite', () => {
 
     it('should have all children', () => {
         const todo = shallow(<Todo classes={{}} />);
+
         expect(todo.find(SearchTodo)).toBeTruthy();
         expect(todo.find(AddTodo)).toBeTruthy();
+        expect(todo.find(TodoList)).toBeTruthy();
     });
 
 });
