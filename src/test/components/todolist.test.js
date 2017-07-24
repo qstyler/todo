421fb7ea0421a36a7c777af222e83ff3f2503879
Kosta <kest.bodnya@gmail.com>
2017-07-24 21:27:38 +0200

    todolist updates to use redux

--- src/test/components/todolist.test.js
+++ src/test/components/todolist.test.js
@@ -6,7 +6,7 @@ import { TodoItem } from '../../components/Todo';
 
 describe('Todolist test suite', () => {
     it('Todolist should be true', () => {
-        expect(shallow(<TodoList classes={{}} todos={[]} />)).toBeTruthy();
+        expect(shallow(<TodoList classes={{}} todos={[]} searchText="" />)).toBeTruthy();
     });
 
     it('should have TodoItems rendered', () => {
@@ -14,7 +14,7 @@ describe('Todolist test suite', () => {
             { id: '1', completed: false, text: 'text' },
             { id: '2', completed: false, text: 'text 2' },
         ];
-        const wrapper = shallow(<TodoList classes={{}} todos={todos} />);
+        const wrapper = shallow(<TodoList classes={{}} todos={todos} searchText="" />);
         expect(wrapper.find(TodoItem)).toHaveLength(todos.length);
     });
 
@@ -26,7 +26,7 @@ describe('Todolist test suite', () => {
                 { id: '2', completed: true, text: 'text 2' },
             ];
 
-            const wrapper = shallow(<TodoList classes={{}} todos={todos} />);
+            const wrapper = shallow(<TodoList classes={{}} todos={todos} searchText="" />);
 
             expect(wrapper.find(TodoItem)).toHaveLength(1);
         });
@@ -37,10 +37,11 @@ describe('Todolist test suite', () => {
                 { id: '2', completed: true, text: 'text 2' },
             ];
 
-            const wrapper = shallow(<TodoList classes={{}} todos={todos} searchAll={true} />);
+            const wrapper = shallow(<TodoList classes={{}} todos={todos} searchAll={true} searchText="" />);
 
             expect(wrapper.find(TodoItem)).toHaveLength(todos.length);
         });
+
 
         it('should filter todos when text is passed', () => {
             const todos = [
