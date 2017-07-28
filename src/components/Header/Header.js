17da4fb659df7acea350b5d391b53a90423c3974
Kosta <kest.bodnya@gmail.com>
2017-07-28 11:48:11 +0200

    dispatch startLogout on logout click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { TopBar, TopBarTitle, TopBarRight, Menu, MenuItem } from 'react-foundation';
 
+import { startLogout } from '../../actions/actions';
+
 import injectSheet from './Styles';
 
 class Header extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        this.props.dispatch(startLogout());
+    }
+
     render() {
         const { title, classes } = this.props;
         return (
@@ -16,7 +30,7 @@ class Header extends Component {
                 <TopBarRight>
                     <Menu className={classes.titleMenu}>
                         <MenuItem>
-                            <a href="">Logout</a>
+                            <a onClick={this.handleLogout}>Logout</a>
                         </MenuItem>
                     </Menu>
                 </TopBarRight>
@@ -30,4 +44,4 @@ Header.propTypes = {
 };
 
 
-export default injectSheet(Header);
+export default connect()(injectSheet(Header));
